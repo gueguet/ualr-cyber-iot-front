d7de168bb2afc2a31d06f44aeed54f7ce610f62a
gueguet57 <gaetan.verdinpol@gmail.com>
2019-06-01 19:28:42 -0500

    Add content to the front

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    accData = {
+                'xacc': 100,
+                'yacc': 2000,
+                'zacc': 2350,
+            }
+
+    return render_template('index.html', accData=accData)
+
+
+# @app.route('/processjson', methods=['POST'])
+# def processjson():
+
+#     data = request.get_json()
+
+#     xacc = data['xacc']
+#     yacc = data['yacc']
+#     zacc = data['zacc']
+
+#     return jsonify({'xacc' : xacc, 'yacc' : yacc, 'zacc' : zacc})
--- static/script.js
+++ static/script.js
@@ -1,1 +1,62 @@
-alert("yo maguele");
+// GAUGE 
+
+
+$(document).ready(function(){
+
+  // alert("fiwejfejwfjweio");
+
+  // console.log("test");
+
+  // var accData = JSON.parse('{{ accData | tojson | safe}}');
+
+  // alert("yo");
+
+  // var accData = JSON.parse('{{ accData | tojson | safe}}');
+  // alert(accData.yacc);
+  
+  // var opts = {
+  //   angle: 0.1, // The span of the gauge arc
+  //   lineWidth: 0.1, // The line thickness
+  //   radiusScale: 1, // Relative radius
+  //   pointer: {
+  //     length: 0.4, // // Relative to gauge radius
+  //     strokeWidth: 0.03, // The thickness
+  //     color: '#000000' // Fill color
+  //   },
+  //   limitMax: false,     // If false, max value increases automatically if value > maxValue
+  //   limitMin: false,     // If true, the min value of the gauge will be fixed
+  //   colorStart: '#6FADCF',   // Colors
+  //   colorStop: '#8FC0DA',    // just experiment with them
+  //   strokeColor: '#E0E0E0',  // to see which ones work best for you
+  //   generateGradient: true,
+  //   highDpiSupport: true,     // High resolution support
+  // }
+  
+  // var targetXGauge = document.getElementById('xGauge'); // your canvas element
+  // var accXGauge = new Gauge(targetXGauge).setOptions(opts); // create sexy gauge!
+  // accXGauge.maxValue = 3000; // set max gauge value
+  // accXGauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+  // accXGauge.animationSpeed = 50; // set animation speed (32 is default value)
+  // accXGauge.set(2000); // set actual value
+
+
+  // var targetYGauge = document.getElementById('yGauge'); // your canvas element
+  // var accYGauge = new Gauge(targetYGauge).setOptions(opts); // create sexy gauge!
+  // accYGauge.maxValue = 3000; // set max gauge value
+  // accYGauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+  // accYGauge.animationSpeed = 50; // set animation speed (32 is default value)
+  // accYGauge.set(100); // set actual value
+
+  // var targetZGauge = document.getElementById('zGauge'); // your canvas element
+  // var accZGauge = new Gauge(targetZGauge).setOptions(opts); // create sexy gauge!
+  // accZGauge.maxValue = 3000; // set max gauge value
+  // accZGauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+  // accZGauge.animationSpeed = 50; // set animation speed (32 is default value)
+  // accZGauge.set(1000); // set actual value
+
+});
+
+
+
+
+
