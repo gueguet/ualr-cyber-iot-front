ebaed780022680c1f737842d78f3ed22dd9a340a
gueguet57 <gaetan.verdinpol@gmail.com>
2019-06-25 22:18:23 -0500

    new conf

--- config.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "apiKey": "AIzaSyACjlfd8559iiA3xbW7WItAb_f1gMQ-uIs",
-    "authDomain": "ualr-cybersecurity.firebaseapp.com",
-    "databaseURL": "https://ualr-cybersecurity.firebaseio.com",
-    "projectId": "ualr-cybersecurity",
-    "storageBucket": "ualr-cybersecurity.appspot.com"
-}
